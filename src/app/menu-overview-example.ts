051efd7f8843e4e51d73389ed8ae542a14116e74
robsonataide <robsonataide@gmail.com>
2021-10-12 17:57:14 +0100

    Initial commit

---
+++ src/app/menu-overview-example.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
+
+/**
+ * @title Basic menu
+ */
+@Component({
+  selector: 'menu-overview-example',
+  templateUrl: 'menu-overview-example.html',
+})
+export class MenuOverviewExample {
+  hasBackdrop: boolean;
+  @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
+
+  update() {
+    this.trigger.closeMenu();
+    setTimeout(() => {
+      this.trigger.openMenu();
+    }, 200);
+  }
+}
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */

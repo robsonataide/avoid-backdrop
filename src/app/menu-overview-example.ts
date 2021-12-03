6df3ae8316df9815bd980a413b0f60f243358133
robsonataide <robsonataide@gmail.com>
2021-12-03 09:50:26 +0000

    3 files modified

--- src/app/menu-overview-example.ts
+++ src/app/menu-overview-example.ts
@@ -16,13 +16,6 @@ export class MenuOverviewExample {
 
   constructor(private overlay: OverlayContainer) {}
 
-  update() {
-    this.trigger.closeMenu();
-    setTimeout(() => {
-      this.trigger.openMenu();
-    }, 200);
-  }
-
   showProblem() {
     alert('The problem...');
   }
@@ -50,10 +43,9 @@ export class MenuOverviewExample {
   }
   private removeCloneBackdrop() {
     this.isAvoidClickOutside = false;
-    document.querySelectorAll('.clone').forEach((element) => element.remove());
+    this.overlay
+      .getContainerElement()
+      .querySelectorAll('.clone')
+      .forEach((element) => element.remove());
   }
 }
-
-/**  Copyright 2021 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
